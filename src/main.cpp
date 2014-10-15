df1188d3a959d6a27fcea4807e25558441004aee
James Durand <james.durand@alumni.msoe.edu>
2014-10-15 16:30:03 -0500

    Fix command line usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 #include "main.h"
 
 #include <boost/program_options.hpp>
+#include <iostream>
 
 namespace po = boost::program_options;
+using namespace std;
 
 int main(int argc, char **argv)
 {
-	po::options_description desc("Simplicity window manager");
+	po::options_description program_desc("Simplicity window manager");
+
+	program_desc.add_options()
+		("help", "Display usage")
+	;
+
+	po::variables_map args;
+	po::store(po::parse_command_line(argc, argv, program_desc), args);
+	po::notify(args);
+
+	if (args.count("help"))
+	{
+		cout << program_desc << endl;
+		return 1;
+	}
 
 	return 0;
 }
