b2ea9a9337b0e9a345782fa3f0c65a4f3411ad39
James Durand <james.durand@alumni.msoe.edu>
2014-10-20 18:35:10 -0500

    Add logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include "main.h"
 
+#include <boost/log/attributes.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
 #include <boost/program_options.hpp>
 #include <glibmm/main.h>
 #include <iostream>
 
-namespace po = boost::program_options;
+namespace logging = boost::log;
+namespace po      = boost::program_options;
 using namespace std;
 
+static logging::sources::severity_logger<logging::trivial::severity_level> GLOBAL_LOGGER;
+
+void init_logging(void);
 void print_version(void);
 
 int main(int argc, char **argv)
@@ -34,11 +45,38 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	init_logging();
+
+	BOOST_LOG_SEV(GLOBAL_LOGGER, logging::trivial::debug) << "Starting main GLib loop";
+
 	Glib::RefPtr<Glib::MainLoop> pMainLoop(Glib::MainLoop::create());
 	pMainLoop->run();
 	pMainLoop->quit();
 
+	BOOST_LOG_SEV(GLOBAL_LOGGER, logging::trivial::debug) << "Ending main GLib loop";
+
 	return 0;
+}
+
+logging::sources::severity_logger<logging::trivial::severity_level> &get_global_logger(void)
+{
+	return GLOBAL_LOGGER;
+}
+
+void init_logging(void)
+{
+	logging::add_common_attributes();
+
+	logging::add_console_log(
+		cout,
+		logging::keywords::format = (
+			logging::expressions::stream
+				<< "(" << logging::expressions::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S") << ") "
+				<< "[" << logging::trivial::severity << "]: "
+				<< logging::expressions::smessage
+		),
+		logging::keywords::auto_flush = true
+	);
 }
 
 void print_version(void)
@@ -48,4 +86,3 @@ void print_version(void)
 			"This is free software; see the source for copying conditions.\n"
 			"There is NO warranty." << endl;
 }
-
--- src/main.h
+++ src/main.h
@@ -2,5 +2,11 @@
 
 #include "config.h"
 
+#define BOOST_LOG_DYN_LINK
+
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/trivial.hpp>
+
 int main(int argc, char **argv);
+boost::log::sources::severity_logger<boost::log::trivial::severity_level> &get_global_logger(void); // TOOD: Make const
 
