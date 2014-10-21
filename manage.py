b7c58ee2d6191c2016ca135f24a79113067cd356
James Durand <james.durand@alumni.msoe.edu>
2014-10-21 13:05:44 -0500

    Add run server command

--- manage.py
+++ manage.py
@@ -48,6 +48,16 @@ categories: {{ categories }}
 	editor = os.environ.get('EDITOR', 'vim')
 	subprocess.call([editor, filepath])
 
+def run_server(args):
+	subprocess.call(
+		[
+			'bundle',
+			'exec',
+			'jekyll',
+			'serve',
+		]
+	)
+
 def parse_args():
 	parser = argparse.ArgumentParser(description = 'Management tool for the blog')
 
@@ -70,6 +80,12 @@ def parse_args():
 		help    = 'Blog post categories',
 	)
 	create_post_parser.set_defaults(func = create_blog_post)
+
+	run_server_parser = subparsers.add_parser(
+		'server',
+		help = 'Run a development server',
+	)
+	run_server_parser.set_defaults(func = run_server)
 
 	return parser.parse_args()
 
