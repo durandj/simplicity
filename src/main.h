79b4591da81535d9820bda788e0acf15f07e9120
James Durand <james.durand@alumni.msoe.edu>
2014-10-21 16:45:08 -0500

    Remove comment which is impossible :(

--- src/main.h
+++ src/main.h
@@ -8,5 +8,5 @@
 #include <boost/log/trivial.hpp>
 
 int main(int argc, char **argv);
-boost::log::sources::severity_logger<boost::log::trivial::severity_level> &get_global_logger(void); // TOOD: Make const
+boost::log::sources::severity_logger<boost::log::trivial::severity_level> &get_global_logger(void);
 
