8126a6bcdcb4c6f3ef76923d878d969fc61669ac
James Durand <james.durand@alumni.msoe.edu>
2014-12-09 16:46:33 -0600

    Force yes for adding ppa

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:boost-latest/ppa &&
+sudo add-apt-repository -y ppa:boost-latest/ppa &&
 sudo apt-get update &&
 sudo apt-get install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev libboost-all-dev
 
