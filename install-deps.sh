629305ff103fd4f569068d5ccbb32b0556ee4acd
James Durand <james.durand@alumni.msoe.edu>
2014-12-09 15:07:19 -0600

    Attempting to fix boost log dep

--- install-deps.sh
+++ install-deps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sudo apt-get -qq update &&
-sudo apt-get -qq install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev libboost-all-dev
+sudo apt-get -qq install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev libboost-all-dev libbost-log-dev
 
