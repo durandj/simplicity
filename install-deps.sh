1fe84ea3ed79a8f1e013338240006f9a70d60012
James Durand <james.durand@alumni.msoe.edu>
2014-12-09 16:06:30 -0600

    Make more verbose output of installation of deps

--- install-deps.sh
+++ install-deps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt-get -qq update &&
-sudo apt-get -qq install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev libboost-all-dev libboost-log-dev
+sudo apt-get update &&
+sudo apt-get install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev libboost-all-dev libboost-log-dev
 
