5f7c136d73d4f6b404e61be043bb118e6f5a4938
James Durand <james.durand@alumni.msoe.edu>
2014-12-10 08:24:39 -0600

    Try root with bootstrap.sh?

--- install-deps.sh
+++ install-deps.sh
@@ -5,7 +5,7 @@ sudo apt-get -qq install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev
 
 git clone --recursive https://github.com/boostorg/boost.git
 cd boost
-./bootstrap.sh --prefix=/usr
+sudo ./bootstrap.sh --prefix=/usr
 sudo ./b2 install --prefix=/usr
 cd ..
 
