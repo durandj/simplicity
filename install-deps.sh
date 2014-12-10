bbb0f083d1f423cd69fb79d2de2d5c35c1a1f514
James Durand <james.durand@alumni.msoe.edu>
2014-12-10 16:58:13 -0600

    Revert back to using apt version of boost

--- install-deps.sh
+++ install-deps.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
 sudo apt-get -qq update &&
-sudo apt-get -qq install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev
-
-git clone --recursive https://github.com/boostorg/boost.git
-cd boost
-sudo ./bootstrap.sh --prefix=/usr
-sudo ./b2 install --prefix=/usr
-cd ..
+sudo apt-get -qq install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev libboost-all-dev
 
