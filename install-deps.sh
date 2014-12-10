03a8d425e23050b4cef9a50ebc83501bde901b7c
James Durand <james.durand@alumni.msoe.edu>
2014-12-09 18:32:49 -0600

    Make installation quiet again

--- install-deps.sh
+++ install-deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt-get update &&
-sudo apt-get install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev
+sudo apt-get -qq update &&
+sudo apt-get -qq install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev
 
 git clone --recursive https://github.com/boostorg/boost.git
 cd boost
