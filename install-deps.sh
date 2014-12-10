191f0ab9b02795d372faa32fe97ac32f59b2f3fd
James Durand <james.durand@alumni.msoe.edu>
2014-12-10 08:09:28 -0600

    Try and force installation to /usr

--- install-deps.sh
+++ install-deps.sh
@@ -5,7 +5,7 @@ sudo apt-get -qq install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev
 
 git clone --recursive https://github.com/boostorg/boost.git
 cd boost
-./bootstrap.sh
-sudo ./b2 install
+./bootstrap.sh --prefix=/usr
+sudo ./b2 install --prefix=/usr
 cd ..
 
