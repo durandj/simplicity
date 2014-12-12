8e1f59116f2d980bf30b1c2949879676bb2b446e
James Durand <james.durand@alumni.msoe.edu>
2014-12-12 09:04:04 -0600

    Fix install-deps spelling error

--- install-deps.sh
+++ install-deps.sh
@@ -5,7 +5,7 @@ sudo apt-get -qq install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev
 
 git clone --recursive --quiet https://github.com/boostorg/boost.git
 cd boost
-./bootstrap --prefix=/usr
+./bootstrap.sh --prefix=/usr
 sudo ./b2 --prefix=/usr link=shared threading=multi address-model=64 architecture=x86
 cd ..
 rm -rf boost
