ce115dd8bbe143cf5c8d1c2057374e1bedf957d8
James Durand <james.durand@alumni.msoe.edu>
2014-12-12 08:56:19 -0600

    Try installing boost from git with more build options

--- install-deps.sh
+++ install-deps.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 sudo apt-get -qq update &&
-sudo apt-get -qq install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev libboost-all-dev
+sudo apt-get -qq install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev
+
+git clone --recursive --quiet https://github.com/boostorg/boost.git
+cd boost
+./bootstrap --prefix=/usr
+sudo ./b2 --prefix=/usr link=shared threading=multi address-model=64 architecture=x86
+cd ..
+rm -rf boost
 
