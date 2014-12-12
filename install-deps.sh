7c7e9ee957f0bf5b365ab82cb21bf5ab6c18eeb3
James Durand <james.durand@alumni.msoe.edu>
2014-12-12 10:09:54 -0600

    Download the 1.57.0 release of boost from boost.org

--- install-deps.sh
+++ install-deps.sh
@@ -3,10 +3,12 @@
 sudo apt-get -qq update &&
 sudo apt-get -qq install gettext xorg-dev libx11-xcb-dev libxcb-util0-dev
 
-git clone --recursive --quiet https://github.com/boostorg/boost.git
+wget -O boost_1_57_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.gz/download
+tar xvf boost_1_57_0.tar.gz
+mv boost_1_57_0 boost
 cd boost
 ./bootstrap.sh --prefix=/usr
 sudo ./b2 --prefix=/usr link=shared threading=multi address-model=64 architecture=x86
 cd ..
-rm -rf boost
+sudo rm -rf boost boost_1_57_0.tar.gz
 
